b54ab8fa1f0be9fe0f959e4dc0f5faa0b3e0b606
Suhaemi  Fahri <121496532+Suhfah@users.noreply.github.com>
2023-01-01 14:20:15 +0700

    Add files via upload

---
+++ Deskripsi.cpp
@@ -0,0 +1,41 @@
+// Deskripsi sembarang berkas dengan
+// algoritma XOR sederhana
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+#include <fstream>
+using namespace std;
+
+main (int argc, char *argv[])
+{
+    FILE *Fin, * fout;
+    char p,c;
+    string K;
+    int i;
+
+    Fin = fopen (argv[1], "rb") ;
+    if (Fin == NULL)
+    {
+        cout << "Berkas " << argv[1] << " tidak ada " << endl;
+    exit(0) ;
+    }
+
+    fout = fopen (argv[2], "wb");
+
+    cout << "Kata Kunci : ";
+    cin  >> K;
+    cout << "Enkripsi " << argv[2] << "...";
+    i = 0;
+    while (!feof(Fin))
+    {
+    p = getc (Fin);
+    c = p ^ K[i];
+
+    // Operasi XOR
+    putc(c, fout);
+    i = (i + 1) % K.length();
+    }
+    fclose(Fin);
+    fclose(fout);
+    }
+
